03abfd46121329ae29737eb0d30b7c328494d389
kukrik <tiit.papp@gmail.com>
2020-10-02 01:17:02 +0300

    Create Toastr.cfg.php

---
+++ install/project/includes/configuration/active/Toastr.cfg.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * toastr defines
+ */
+
+define('QCUBED_TOASTR_URL', dirname(QCUBED_PROJECT_DIR) . '/vendor/kukrik/toastr');
+define('QCUBED_TOASTR_ASSETS_URL', dirname(QCUBED_BASE_URL) . '/kukrik/toastr/assets');
+
+
+
+
